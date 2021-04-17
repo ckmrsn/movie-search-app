1f792e80b842f9908087e94489e09009f0a56e55
Ersin <ckmrsn@gmail.com>
2021-04-18 02:48:33 +0300

    patika homework completed

--- src/config/Router.js
+++ src/config/Router.js
@@ -1,20 +1,27 @@
-import Home from '../pages/Home';
-import Favorite from '../pages/Favorite';
-import Detail from '../pages/Detail';
+import Home from '../pages/Home'
+import Favorite from '../pages/Favorite'
+import Detail from '../pages/Detail'
 
 export const routes = [
-  {
-    path: '/',
-    exact: true,
-    component: <Home />,
-    title: 'Home',
-    isHeaderElement: true,
-  },
-  {
-    path: '/movies/:id',
-    exact: false,
-    component: <Detail />,
-    title: 'Detail',
-    isHeaderElement: false,
-  },
-];
+	{
+		path: '/',
+		exact: true,
+		component: <Home />,
+		title: 'Home',
+		isHeaderElement: true,
+	},
+	{
+		path: '/favorite',
+		exact: true,
+		component: <Favorite />,
+		title: 'Favorite',
+		isHeaderElement: true,
+	},
+	{
+		path: '/movies/:id',
+		exact: false,
+		component: <Detail />,
+		title: 'Detail',
+		isHeaderElement: false,
+	},
+]
